46d09ecb19d4ccb196bc0050c184746575b43998
Chao Zhu <34178154+Super1ZC@users.noreply.github.com>
2018-03-02 13:48:13 +0800

    跟login.html相关的工作 (#25)
    * fix #16 finish base.html
    
    * modify base.html
    
    * work relates to login.html

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,6 +1,23 @@
 from flask import Blueprint, render_template
+from jobplus.forms import LoginForm, CompanyRegisterForm, EmployeeRegisterForm
 
 front = Blueprint('front', __name__)
 @front.route('/')
 def index():
 	return render_template('index.html')
+
+@front.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html',form=form)
+
+@front.route('/company_register')
+def company_register():
+	form = CompanyRegisterForm()
+	return render_template('company_register.html',form=form)
+
+@front.route('/employee_register')
+def employee_register():
+	form = EmployeeRegisterForm()
+	return render_template('employee_register.html',form=form)
+	
